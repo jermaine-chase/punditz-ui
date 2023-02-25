da19dc1cc55ae7473094cbac1f2f37bd699b1e0c
JermS <jermsama@gmail.com>
2023-02-24 23:17:53 -0500

    Login And Registration Working

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
+import {AuthService, IUser} from "../../services/auth.service";
 
 @Component({
   selector: 'app-navbar',
@@ -8,9 +9,31 @@ import {Router} from "@angular/router";
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private router: Router) { }
-
-  ngOnInit(): void {
+  isAuthenticated: boolean;
+  user: IUser;
+  constructor(private router: Router, private cognitoService: AuthService) {
+    this.isAuthenticated = false;
+    this.user = {} as IUser;
   }
 
+  ngOnInit(): void {
+    this.cognitoService.isAuthenticated()
+      .then(async (success: boolean) => {
+        this.isAuthenticated = success;
+        if (success) {
+          this.user = await this.cognitoService.getCurrentAuthenticatedUser();
+        }
+      });
+  }
+
+  public signOut(): void {
+    this.cognitoService.signOut()
+      .then(() => {
+        this.router.navigate(['/signIn']);
+      });
+  }
+
+  isAdmin() {
+    return this.cognitoService.isAdmin()
+  }
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { IUser, AuthService } from "../../services/auth.service";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent{
 
-  constructor() { }
+  confirm: boolean;
+  loading: boolean;
+  user: IUser;
 
-  ngOnInit(): void {
+  constructor(private router: Router, private cognitoService: AuthService) {
+    this.confirm = false;
+    this.loading = false;
+    this.user = {admin: false} as IUser;
+  }
+
+  public signUp(): void {
+    this.loading = true;
+    this.cognitoService.signUp(this.user)
+      .then(() => {
+        this.loading = false;
+        this.confirm = true;
+      }).catch((error) => {
+        console.log(error);
+      this.loading = false;
+    });
+  }
+
+  public confirmSignUp(): void {
+    this.loading = true;
+    this.cognitoService.confirmSignUp(this.user)
+      .then(() => {
+        this.router.navigate(['/login']);
+      }).catch(() => {
+      this.loading = false;
+    });
   }
 
 }

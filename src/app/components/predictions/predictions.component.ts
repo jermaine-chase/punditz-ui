6a403c587062a97d34a6a443c0e9e81783ddde60
JermS <jermsama@gmail.com>
2023-04-07 22:33:39 -0400

    quick commit

--- src/app/components/predictions/predictions.component.ts
+++ src/app/components/predictions/predictions.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PredictionsForm} from "../../shared/forms";
 
 @Component({
@@ -12,32 +12,43 @@ export class PredictionsComponent implements OnInit {
     this.predictionsForm = {
       teams: ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
     }
-
-    this.usedTeams = []
-
-    this.teamsList = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
   }
 
-  teamsList: string[][];
-  usedTeams: string[]
-  allTeams = ['Arsenal FC', 'Aston Villa FC', 'Chelsea FC', 'Everton FC', 'Fulham FC', 'Liverpool FC', 'Manchester City FC', 'Manchester United FC', 'Newcastle United FC', 'Tottenham Hotspur FC', 'Wolverhampton Wanderers FC', 'Leicester City FC', 'Southampton FC', 'Leeds United FC', 'Nottingham Forest FC', 'Crystal Palace FC', 'Brighton &amp; Hove Albion FC', 'Brentford FC', 'West Ham United FC', 'AFC Bournemouth']
+  loadedFromDb = false
+
+  allTeams = ['Arsenal FC', 'Aston Villa FC', 'Chelsea FC', 'Everton FC', 'Fulham FC', 'Liverpool FC', 'Manchester City FC', 'Manchester United FC', 'Newcastle United FC', 'Tottenham Hotspur FC', 'Wolverhampton Wanderers FC', 'Leicester City FC', 'Southampton FC', 'Leeds United FC', 'Nottingham Forest FC', 'Crystal Palace FC', 'Brighton & Hove Albion FC', 'Brentford FC', 'West Ham United FC', 'AFC Bournemouth']
   predictionsForm: PredictionsForm;
 
   ngOnInit(): void {
   }
 
-  populateTeams(rank: number) {
-    let value = this.predictionsForm.teams[rank]
-    this.teamsList[rank] = this.allTeams
-      .filter(c => {
-        console.log(c, value)
-        c.startsWith(value)
-      })
-      .slice(0, 5);
+  chooseTeam(team: string, idx: number) {
+    for (let i in this.predictionsForm.teams) {
+      if (this.predictionsForm.teams[i] === '') {
+        this.predictionsForm.teams[i] = team;
+        break;
+      }
+    }
+    this.allTeams.splice(idx, 1);
   }
 
-  selectOption(i: number, s: string) {
-    this.predictionsForm.teams[i] = s;
-    this.teamsList[i] = [];
+  removeTeam(index: number) {
+    this.allTeams.push(this.predictionsForm.teams[index]);
+    this.predictionsForm.teams.splice(index, 1);
+    this.predictionsForm.teams.push('');
+  }
+
+  submit() {
+    let dbObject = {
+      teams: this.predictionsForm.teams
+    }
+    this.loadedFromDb = true;
+  }
+
+  reset() {
+    this.allTeams = ['Arsenal FC', 'Aston Villa FC', 'Chelsea FC', 'Everton FC', 'Fulham FC', 'Liverpool FC', 'Manchester City FC', 'Manchester United FC', 'Newcastle United FC', 'Tottenham Hotspur FC', 'Wolverhampton Wanderers FC', 'Leicester City FC', 'Southampton FC', 'Leeds United FC', 'Nottingham Forest FC', 'Crystal Palace FC', 'Brighton & Hove Albion FC', 'Brentford FC', 'West Ham United FC', 'AFC Bournemouth']
+    this.predictionsForm = {
+      teams: ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
+    }
   }
 }
--- src/app/shared/shared.service.ts
+++ src/app/shared/shared.service.ts
@@ -2,21 +2,33 @@ import { Injectable } from '@angular/core';
 import {MatchDTO, TeamDTO} from "../model/MatchResponseInterface";
 import {BackendService} from "../services/backend.service";
 import {IUser} from "../services/auth.service";
+import {CycleDTO} from "../model/CycleDTO";
+import {AwsService} from "../services/aws.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
-  getTeamsMap(): Map<number, TeamDTO> {
-    return this.teamsMap;
-  }
+  private loadingTeams = false
+  private loadingMatches = false
   private teams : TeamDTO[] = [];
   private matches: MatchDTO[] = [];
-  private teamsMap = new Map<number, TeamDTO>();
+  private cycles: CycleDTO[] = []
+  private currentCycle = 0
+  teamsMap = new Map<number, TeamDTO>();
+  matchesMap = new Map<number, MatchDTO>();
   private user: IUser | undefined;
-  constructor(private backend: BackendService) {
-    this.populateTeams()
-    this.populateMatches()
+  constructor(private backend: BackendService, private awsService: AwsService) {
+    this.populateTeams().then(()=> {
+      console.log("SharedService::constructor() - Teams Loaded")
+      this.loadingTeams = false;
+    })
+    this.populateMatches().then(()=>{
+      console.log("SharedService::constructor() - Matches Loaded")
+      this.loadingMatches = false
+      this.populateCycles().then(()=>console.log("SharedService::constructor() - Cycles Loaded"))
+    })
+
   }
   retrieveTeams() {
     return this.backend.getTeams(2021)
@@ -28,9 +40,14 @@ export class SharedService {
 
   setTeams(teams: TeamDTO[]) {
     this.teams = teams
+    localStorage.setItem('teams', JSON.stringify(teams))
   }
 
   getMatches() {
+    if (this.matches?.length == 0) {
+      this.populateTeams().then(()=>console.log("SharedService::getMatches() - Teams Loaded"))
+      this.populateMatches().then(()=>console.log("SharedService::getMatches() - Matches Loaded"))
+    }
     return this.matches
   }
 
@@ -39,33 +56,105 @@ export class SharedService {
   }
 
   async populateMatches() {
+    if (this.loadingMatches) {
+      console.log('matches load in progress')
+      return
+    }
+    this.loadingMatches = true
     if (this.matches?.length > 0) {
       console.log('matches already populated')
       return
     }
-    let data = await this.retrieveMatches()
-    for (let match of data.matches!) {
+    const m = localStorage.getItem('matches')
+    if (m) {
+      this.matches = JSON.parse(m)
+    } else {
+      let data = await this.retrieveMatches()
+      for (let match of data.matches!) {
         let ht = match.homeTeam
         let at = match.awayTeam
         match.localDate = new Date(match.utcDate).toLocaleString();
         ht.tla = this.teamsMap.get(ht.id)?.tla
         at.tla = this.teamsMap.get(at.id)?.tla
+        if (!this.currentCycle || this.currentCycle === 0) {
+          this.currentCycle = match.season.currentMatchday
+        }
+      }
+      this.setMatches(data.matches!)
+      this.matchesMap = new Map<number, MatchDTO>(this.matches.map((obj: MatchDTO) => [obj.id, obj]))
     }
-    this.setMatches(data.matches!)
   }
 
   async populateTeams() {
+    if (this.loadingTeams) {
+      console.log('teams load in progress')
+      return
+    }
+    this.loadingTeams = true
     if (this.teams?.length > 0) {
       console.log('teams already populated')
       return
     }
-    let data = await this.retrieveTeams()
-    this.setTeams(data.teams!);
-    this.teamsMap = new Map<number, TeamDTO>(data.teams!.map((obj: TeamDTO) => [obj.id, obj]))
+    const t = localStorage.getItem('teams')
+    if (t) {
+      this.teams = JSON.parse(t)
+    } else {
+      let data = await this.retrieveTeams()
+      this.setTeams(data.teams!);
+    }
+    this.teamsMap = new Map<number, TeamDTO>(this.teams.map((obj: TeamDTO) => [obj.id, obj]))
+  }
+
+  async populateCycles() {
+    if (this.cycles?.length > 0) {
+      console.log('cycles already populated')
+      return
+    }
+    const c = localStorage.getItem('cycles')
+    if (c) {
+      this.cycles = JSON.parse(c)
+      return
+    } else {
+      for (let m of this.getMatches()) {
+        const start = new Date(m.utcDate)
+        const end = new Date(m.utcDate)
+        let c: CycleDTO = {
+          cycleNumber: m.matchday,
+          matches: [m.id],
+          start: start,
+          end: end
+        }
+        // found already, so add match, update start and end for cycle
+        if (this.cycles[m.matchday]) {
+          c = this.cycles[m.matchday]
+          c.matches.push(m.id)
+          if (c.start > start) c.start = start
+          if (c.end < end) c.end = end
+          // not found, add
+        } else {
+          this.cycles[m.matchday] = c
+        }
+      }
+      localStorage.setItem('cycles', JSON.stringify(this.cycles))
+    }
+  }
+
+  getCurrentCycle() {
+    return this.getCycle(this.currentCycle)
+  }
+
+  getCycle(cycleNumber: number) {
+    if (this.cycles?.length === 0) {
+      this.populateCycles();
+    }
+    const matchingCycles = this.cycles.filter(c=>c.cycleNumber === cycleNumber)
+    // assumption: there can be only one!
+    return matchingCycles.pop()!!
   }
 
   setMatches(matches: MatchDTO[] ) {
     this.matches = matches
+    localStorage.setItem('matches', JSON.stringify(matches))
   }
   getUser() {
     return this.user
@@ -73,6 +162,7 @@ export class SharedService {
 
   setUser(user: IUser) {
     this.user = user
+    localStorage.setItem('user', JSON.stringify(user))
   }
 
   populateUser(result: any): IUser {
@@ -84,5 +174,25 @@ export class SharedService {
     user.firstName = result.attributes['custom:lastName'];
 
     return user;
+  }
+
+  parseAwsResponse(response: any): any[] {
+    let r: any[] = []
+    if (response?.Count > 0) {
+      r = response.Items
+    } else if (response?.Attributes) {
+      r.push(response.Attributes)
+    }
+    return r
+  }
+
+  async getAwsObjects(type: string, cycle: number): Promise<any[]> {
+    let rawResponse = await this.awsService.getFromDb('/' + type + '/' + cycle)
+    return this.parseAwsResponse(rawResponse)
+  }
+
+  async saveAwsObject(type: string, object: any) {
+    let rawResponse = await this.awsService.saveToDb('/' + type, object)
+    return this.parseAwsResponse(rawResponse)
   }
 }

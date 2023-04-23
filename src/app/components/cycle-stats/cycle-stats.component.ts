34254f8b79cf91cd94eba7a680215ee2840224ff
JermS <jermsama@gmail.com>
2023-04-23 01:00:58 -0400

    Pushing to AWS in flight

--- src/app/components/cycle-stats/cycle-stats.component.ts
+++ src/app/components/cycle-stats/cycle-stats.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BackendService} from "../../services/backend.service";
+import {SharedService} from "../../shared/shared.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-cycle-stats',
@@ -9,11 +11,32 @@ import { BackendService} from "../../services/backend.service";
 export class CycleStatsComponent implements OnInit {
   url = '/cycle/'
   cycle: any;
+  private loading: boolean;
+  private cyclePicker: number = 0;
 
-  constructor(private backend: BackendService) { }
+  constructor(private router: Router, private route: ActivatedRoute,
+              private backend: BackendService, private share: SharedService) {
+    this.loading = true;
+    this.route.params.subscribe(params => this.cyclePicker = params['cycle']?parseInt(params['cycle'], 10):this.cycle.cycleNumber)
+    this.share.populateTeams().then(
+      ()=>{
+        this.share.populateMatches().then(
+          ()=> {
+            // this.teams = this.share.getTeams()
+            this.cycle = this.share.getCurrentCycle()
+            this.loading = false;
+            // this.setForm();
+          }
+        )
+      }
+    );
+  }
 
   ngOnInit(): void {
-    this.cycle = this.backend.getCycles()
+    //this.cycle = this.backend.getCycles()
+    if (this.cyclePicker === 0) {
+      this.cyclePicker = this.cycle.cycleNumber;
+    }
   }
 
   cycleCount() {

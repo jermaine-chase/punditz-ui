6c03c16a101d6f83a59cbc0fc2e3789d2939d897
JermS <jermsama@gmail.com>
2023-04-23 01:01:12 -0400

    Pushing to AWS in flight

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { IUser, AuthService } from "../../services/auth.service";
+import {AwsService} from "../../services/aws.service";
 
 @Component({
   selector: 'app-register',
@@ -14,7 +15,7 @@ export class RegisterComponent{
   loading: boolean;
   user: IUser;
 
-  constructor(private router: Router, private cognitoService: AuthService) {
+  constructor(private router: Router, private cognitoService: AuthService, private aws: AwsService) {
     this.confirm = false;
     this.loading = false;
     this.user = {admin: false} as IUser;
@@ -23,9 +24,10 @@ export class RegisterComponent{
   public signUp(): void {
     this.loading = true;
     this.cognitoService.signUp(this.user)
-      .then(() => {
-        this.loading = false;
-        this.confirm = true;
+      .then(() => {;
+        this.aws.saveToDb('/user', {'username': this.user.username}).then(()=> {
+          this.loading = false;
+        })
       }).catch((error) => {
         console.log(error);
       this.loading = false;
@@ -36,6 +38,7 @@ export class RegisterComponent{
     this.loading = true;
     this.cognitoService.confirmSignUp(this.user)
       .then(() => {
+        this.confirm = true;
         this.router.navigate(['/login']);
       }).catch(() => {
       this.loading = false;
